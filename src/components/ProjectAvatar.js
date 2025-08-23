bc23640c0e5096273094f65beab0f5884c097401
jazbelrose <120759784+jazbelrose@users.noreply.github.com>
2025-08-22 21:15:04 -0700

    refactor: migrate components and utilities to typescript

--- src/components/ProjectAvatar.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import SVGThumbnail from '../pages/dashboard/components/AllProjects/SVGThumbnail';
-const ProjectAvatar = ({ thumb, name = '', initial = '', className = '' }) => (thumb ? (_jsx("img", { src: thumb, alt: name, className: className })) : (_jsx(SVGThumbnail, { initial: initial || name.charAt(0).toUpperCase(), className: className })));
-export default ProjectAvatar;
