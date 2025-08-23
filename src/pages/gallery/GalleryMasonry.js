ada4e9d4fa0bdfd0aae0777803e8972aa2ce0e37
jazbelrose <120759784+jazbelrose@users.noreply.github.com>
2025-08-23 14:48:30 -0700

    chore: migrate remaining components to typescript

--- src/pages/gallery/GalleryMasonry.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import styles from './GalleryMasonry.module.css';
-const GalleryMasonry = ({ imageUrls = [], onImageClick = () => { } }) => {
-    return (_jsx("div", { className: styles.masonry, "data-testid": "gallery-masonry", children: imageUrls.map((src, idx) => (_jsx("img", { src: src, alt: `Gallery item ${idx + 1}`, className: styles.image, onClick: () => onImageClick(idx) }, idx))) }));
-};
-export default GalleryMasonry;
